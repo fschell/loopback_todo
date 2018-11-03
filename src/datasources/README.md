b23b865e6924668a0aecf3df1bc4f9132bc8a29d
fs <fs@imagine-orange.com>
2018-11-03 12:08:38 +0100

    initial commit from running todo example with geocoder

---
+++ src/datasources/README.md
@@ -0,0 +1,10 @@
+# Datasources
+
+This directory contains config for datasources used by this app.
+
+## geocode
+A valid address is a string in the form:
+
+"1600 Pennsylvania Ave NW, , , 20502"
+
+see also: https://www.census.gov/data/developers/data-sets/Geocoding-services.html
---
+++ src/models/todo.model.ts
@@ -0,0 +1,35 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+    @property({
+        type: 'number',
+        id: true,
+    })
+    id?: number;
+
+    @property({
+        type: 'string',
+        required: true,
+    })
+    title: string;
+
+    @property({
+        type: 'boolean',
+    })
+    isCompleted?: boolean;
+
+    @property({
+        type: 'string',
+    })
+    remindAtAddress?: string; // address,city,zipcode
+
+    @property({
+        type: 'string',
+    })
+    remindAtGeo?: string; // latitude,longitude
+
+    constructor(data?: Partial<Todo>) {
+        super(data);
+    }
+}
